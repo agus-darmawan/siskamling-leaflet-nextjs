e589800377ccafa3c448b1b63f48af606136a1c5
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-09 18:50:20 +0700

    feat: new api convention

--- src/app/api/submit-report/route.ts
+++ src/app/api/submit-report/route.ts
@@ -1,18 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse, NextRequest } from 'next/server';
 import createReport from '@/lib/reportService';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-
+export async function POST(req: NextRequest, res: NextResponse) {
   const reportData = req.body;
-
   try {
     const report = await createReport(reportData);
-    return res.status(201).json({ report });
-  } catch (error) {
-    console.error('Error creating report:', error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+    return NextResponse.json({
+      success: true,
+      message: 'Welcome to Agus Darmawan Website!',
+      data: report
+    });
+  }catch (e) {
+    return NextResponse.json({
+      success: false,
+      message: 'Something went wrong ! Please try again',
+    });
   }
 }
