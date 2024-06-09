f7e7cc546333fc3edb79963dd12d69f0db031278
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-09 18:43:31 +0700

    fix: deprecated

--- src/app/api/submit-report/route.ts
+++ src/app/api/submit-report/route.ts
@@ -1,23 +1,18 @@
-import createReport from '@/lib/reportService';
 import { NextApiRequest, NextApiResponse } from 'next';
+import createReport from '@/lib/reportService';
 
-export const config = {
-  api: {
-    externalResolver: true,
-  },
-};
-
-export default async function handler(req : NextApiRequest, res : NextApiResponse) {
-  if (req.method === 'POST') {
-    const reportData = req.body;
-    try {
-      const report = await createReport(reportData);
-      return res.status(201).json({ report });
-    } catch (error) {
-      console.error('Error creating report:', error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
-  } else {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  const reportData = req.body;
+
+  try {
+    const report = await createReport(reportData);
+    return res.status(201).json({ report });
+  } catch (error) {
+    console.error('Error creating report:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 }
