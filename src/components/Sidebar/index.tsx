91ba930d29c77775f27f71da1e33af2734004720
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-09 14:27:34 +0700

    feat: add image in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { IoIosCreate } from "react-icons/io";
 import { TbMapSearch } from "react-icons/tb";
 import { VscGraphLine } from "react-icons/vsc";
@@ -14,28 +15,45 @@ const Sidebar: React.FC = () => {
       className="fixed inset-y-0 left-0 bg-[#0C364B] transform transition-transform translate-x-0 z-20 w-64 border-r mt-16"
       tabIndex={-1}
     >
-      <div className="w-64 p-4">
-        <div className="flex justify-between items-center pt-5">
-          <h5 className="text-xl font-bold text-[#9BD2EF]">Beranda</h5>
+      <div className="w-64 p-4 h-full flex justify-between flex-col">
+        <div>
+          <div className="flex justify-between items-center pt-5">
+            <h5 className="text-lg font-bold text-[#9BD2EF]">Beranda</h5>
+          </div>
+          <div className="mt-4 space-y-2 text-white font-bold">
+            <Navigate
+              href="/Petasebaran"
+              icon={<TbMapSearch />}
+              text="Peta Sebaran"
+              isActive={pathname === "/Petasebaran"}
+            />
+            <Navigate
+              href="/formaduan"
+              icon={<IoIosCreate />}
+              text="Form Aduan"
+              isActive={pathname === "/formaduan"}
+            />
+            <Navigate
+              href="#"
+              icon={<VscGraphLine />}
+              text="Statistika"
+              isActive={pathname === "#"}
+            />
+          </div>
         </div>
-        <div className="mt-4 space-y-4 text-white font-bold">
-          <Navigate
-            href="/Petasebaran"
-            icon={<TbMapSearch />}
-            text="Peta Sebaran"
-            isActive={pathname === "/Petasebaran"}
-          />
-          <Navigate
-            href="/formaduan"
-            icon={<IoIosCreate />}
-            text="Form Aduan"
-            isActive={pathname === "/formaduan"}
-          />
-          <Navigate
-            href="#"
-            icon={<VscGraphLine />}
-            text="Statistika"
-            isActive={pathname === "#"}
+        <div className="space-y-2">
+          <p className="text-white font-semibold text-center leading-tight text-sm">
+            Support by <br /> Institut Teknologi Sepuluh Nopember <br />
+            Geomatics Engineering <br />
+            Field Camp Thematic <br />
+            Team 16
+          </p>
+          <Image
+            src="/images/footer/logo-its-dep-bg.png"
+            alt="Logo"
+            width={151}
+            height={34}
+            className="w-full max-w-xs px-4"
           />
         </div>
       </div>
